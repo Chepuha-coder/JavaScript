4f10ee16c3906cc84d5b844655a86a510683c622
user5321 <wifi5324518@gmail.com>
2022-08-25 02:51:09 +0300

    Half of the project

---
+++ homework-24.08.2022/user-details.js
@@ -0,0 +1,49 @@
+// На странице user-details.html:
+// 4 Вивести всю, без виключення, інформацію про об'єкт user на який клікнули
+// 5 Додати кнопку "post of current user", при кліку на яку, з'являються title всіх постів поточного юзера
+// (для получения постов используйте эндпоинт https://jsonplaceholder.typicode.com/users/USER_ID/posts)
+// 	6 Каждому посту додати кнопку/посилання, при кліку на яку відбувається перехід на сторінку post-details.html, котра має детальну інфу про поточний пост.
+
+let user = JSON.parse(localStorage.getItem("userName")); // {}
+
+function showUser(user) {
+	for (let key in user) {
+
+		let div = document.createElement("div");
+
+		if (typeof user[key] !== "object") {
+			div.innerText = `${key}: ${user[key]}`;
+		} else {
+			showUser(user[key]);
+		}
+
+		document.body.appendChild(div);
+	}
+}
+
+showUser(user);
+
+let button = document.createElement("button");
+button.innerText = "post of current user";
+document.body.appendChild(button);
+
+let id = user.id;
+fetch(`https://jsonplaceholder.typicode.com/users/${id}/posts`)
+.then(response => {
+	return response.json();
+}).then(posts => {
+	button.onclick = function () {
+		for (const post of posts) {
+			let div = document.createElement("div");
+			let postName = document.createElement("a");
+			postName.setAttribute("href", "post-details.html");
+			postName.innerText = post.title;
+			div.appendChild(postName);
+			postName.onclick = function () {
+				localStorage.setItem("post", JSON.stringify(post));
+			}
+			document.body.appendChild(div);
+		}
+	};
+});
+
